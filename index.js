ac2acc1d9c448d2bb19515126052b10bc738712a
github username <github email address>
2017-06-05 11:02:52 -0400

    Done

--- index.js
+++ index.js
@@ -5,6 +5,68 @@ const BUTCHER_PRODUCTS = [
   'Ribeye'
 ];
 
+const OlderCoaster = React.createClass({
+  render() {
+    return React.createElement('div', {className: 'oldercoaster'},
+      React.createElement('p', {}, 'Two grannies having the time of their life!'),
+      React.createElement('p', {}, 'Passengers:'),
+      React.createElement('ul', {},
+        React.createElement('li', {}, 'Agnes'),
+        React.createElement('li', {}, 'Muriel'))
+      )}
+    });
+
+class InFrontOfYou extends React.Component {
+  render() {
+    return React.createElement('div', {},
+    [
+      React.createElement('p', {}, "You shouldn't look too far."),
+      React.createElement('p', {}, 'Sometimes, the solution is ',
+        React.createElement('strong', {}, 'right in front of you.')
+      )
+    ])
+  }
+}
+
+class ButcherShop extends React.Component {
+  render() {
+    return React.createElement('div', {className:'butcher-shop'},
+      [
+        React.createElement('p', {}, 'Hello! We have the following products for sale today:'),
+        React.createElement('ul', {},
+          BUTCHER_PRODUCTS.map(function(product){
+            return React.createElement('li', {}, product)
+          })
+        )
+      ])
+  }
+}
+
+
+// <div class="butcher-shop">
+//   <p>Hello! We have the following products for sale today:</p>
+//   <ul>
+//     <li>Tenderloin</li>
+//     <li>Short ribs</li>
+//     <li>Beef shin</li>
+//     <li>Ribeye</li>
+//   </ul>
+// </div>
+
+// <div>
+//   <p>You shouldn't look too far.</p>
+//   <p>Sometimes, the solution is <strong>right in front of you.</strong></p>
+// </div>
+
+// <div class="oldercoaster">
+//   <p>Two grannies having the time of their life!</p>
+//   <p>Passengers:</p>
+//   <ul>
+//     <li>Agnes</li>
+//     <li>Muriel</li>
+//   </ul>
+// </div>
+
 ReactDOM.render(
   React.createElement('div', {}, [
     React.createElement(OlderCoaster),
